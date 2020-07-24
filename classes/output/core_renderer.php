07da172a687ddf358d46a1d7195c268fefacf46d
Francesc Busquets <francesc@gmail.com>
2020-07-24 11:23:34 +0200

    Make Agora logo conditional
    - El logotip d'Àgora només apareix al peu de pàgina quan $CFG->isagora és cert (això pot ser útil per a Odissea i altres)
    - L'adreça de l'enllaç del logo d'Àgora al peu de pàgina canvia quan $CFG->iseoi

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -142,9 +142,13 @@ class core_renderer extends \core_renderer {
     }
 
     public function footer_logos() {
+        global $CFG;
         $logos = '<a href="http://ensenyament.gencat.cat/ca/inici/" class="brand ensenyament"><img src="'.$this->image_url('departament', 'theme').'" alt="Departament d\'Educació" title="" /></a>';
         $logos .= '<a href="http://xtec.gencat.cat" class="brand xtec"><img src="'.$this->image_url('xtec', 'theme').'" alt="Xarxa Telemàtica Educativa de Catalunya" title="" /></a>';
-        $logos .= '<a href="https://educaciodigital.cat" target="_blank" class="agora_footer"><img src="'.$this->image_url('logo_main', 'theme').'" alt="Eix" title="" /></a>';
+        if (isset($CFG->isagora) && $CFG->isagora) {
+            $href = (isset($CFG->iseoi) && $CFG->iseoi) ? 'https://agora-eoi.xtec.cat/' : 'https://educaciodigital.cat/';
+            $logos .= '<a href="'.$href.'" target="_blank" class="agora_footer"><img src="'.$this->image_url('logo_main', 'theme').'" alt="Eix" title="" /></a>';
+        }
         $logos .= '<a href="https://moodle.org" target="_blank" class="moodle_footer" title="Moodle"><img src="'.$this->image_url('moodlelogo').'" alt="'.get_string('moodlelogo').'"/></a>';
 
         return $logos;
