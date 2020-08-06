330ea2042d11087236d640dbd82f19f065d885f0
Francesc Busquets <francesc@gmail.com>
2020-08-06 10:03:37 +0200

    Allow submenus in custom menu
    En aquesta primera implementació tots els nivells de submenú es mostren desplegats.
    TODO: Mostrar i amagar els diversos nivells de submenú de manera dinàmica.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_xtec2020';
 $plugin->release = '0.1.0';
-$plugin->version = 2020080200;
+$plugin->version = 2020080600;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_ALPHA;
