7172fcdf7101434177891688fcc140597f431d21
Francesc Busquets <francesc@gmail.com>
2020-10-19 13:53:49 +0200

    Added function get_xtec_type

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_xtec2020';
 $plugin->release = '0.1.2';
-$plugin->version = 2020101904;
+$plugin->version = 2020101905;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
