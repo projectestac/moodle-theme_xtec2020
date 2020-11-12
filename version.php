5fdab0282e9a850d6237e33e01cac8c29c6740c4
Francesc Busquets <francesc@gmail.com>
2020-11-12 20:27:16 +0100

    Updated version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_xtec2020';
 $plugin->release = '0.1.2';
-$plugin->version = 2020110400;
+$plugin->version = 2020111300;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
